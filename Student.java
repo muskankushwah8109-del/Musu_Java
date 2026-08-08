374b92afcdd9e2c75cef45ad9abc60d4065f7e34
Muskan <muskankushwah8109@gmail.com>
2026-08-08 12:54:13 +0530

    Added Student program

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    public static void main(String[] args) {
+
+        String name = "Muskan";
+        int age = 20;
+        String course = "B.Tech CSE AI & ML";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+}
+  }
